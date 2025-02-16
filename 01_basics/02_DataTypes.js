6b10a20c4bf2bade8cd47c8d0c3fdb837d0c28d6
TulikaT <66399750+TulikaT@users.noreply.github.com>
2025-02-16 18:44:12 +0000

    commit

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello"); // we are using node js not browser
+
+// console.log(3+3) console.log("hello")  // error 
+
+
+let name = "tt";
+let age=54;
+let isLoggesIn = false;
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);    //object
